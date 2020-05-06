9d0d2d2fd211ba6004717360c0ec265d291afc6d
Administrator <Administrator@WIN-H4NGRS3NN5N>
2020-05-06 00:01:42 -0400

    Initial Checkin

---
+++ Models/CustomListItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace StarshipfleetsAPI.Models
+{
+    public class CustomListItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
